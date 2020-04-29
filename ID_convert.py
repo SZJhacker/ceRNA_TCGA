15b09ab2ce7974b060b2f079720163296257316e
SZJhacker <shenzijie2013@163.com>
2020-04-29 16:24:52 +0800

    正则表达错误，进行修改

--- ID_convert.py
+++ ID_convert.py
@@ -12,14 +12,16 @@ def convert_id(names):
 
 def main():
     parser = argparse.ArgumentParser(description="ID 转换")
-    parser.add_argument('-i', '--infile', help='含有基因id的文件')
-    parser.add_argument('-o', '--outfile', help='输出文件')
+    parser.add_argument('-i', '--infile', help='含有基因id的文件，目前只接受csv格式输入文件')
+    parser.add_argument('-o', '--outfile', help='输出文件，目前输出格式比较单一，输出csv文件')
     args = parser.parse_args()
 
     geneExpr = pd.read_csv(args.infile)
-    geneExpr["ensemble_id"] = geneExpr["ensemble_id"].str.replace(r'\.\d', '') # 去掉ensemble后面的版本号，方法很多，例如列表解析式
+    geneExpr["ensemble_id"] = geneExpr["ensemble_id"].str.replace(r'\.\d+', '') # 去掉ensemble后面的版本号，方法很多，例如列表解析式
     collections_id = convert_id(geneExpr["ensemble_id"])
     geneExpr_converted_id = pd.merge(collections_id, geneExpr, on=['ensemble_id']).fillna("unknown")
     geneExpr_converted_id.to_csv(args.outfile, index=False )
     # collections_id.to_csv(args.outfile)
-main()
+    
+if __name__ == "__main__":
+    main()
